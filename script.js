2bd262f80981c749a834110366c1ea480a32d176
the                                                            Z giant <zionagrate@gmail.com>
2022-08-24 20:07:47 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
-const player = document.querySelector('player')
- player.innerHTML;
-
+const player = document.querySelector('player');
+const CPU = document.querySelector('Computer');
+const resetBtn = document.querySelectorAll('.resetBtn');
+turn = true;
+x = 0;
+y = 0;
+displayTurn();
 function flipCard(){
     if(lockBoard) return;
     if(this === firstCard) return;
@@ -18,25 +22,38 @@ function flipCard(){
     } 
         //second click
         secondCard = this;
-       
         checkForMatch();
 }
-
-console.log(player);
+console.log(resetBtn);
 
 
 function checkForMatch() {
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     console.log(firstCard.dataset.framework)
     isMatch ? disableCards() : unflipCards();
-       
-}            x = 0;
-
+       switchPlayer()
+}       
+function switchPlayer(){
+    if(turn == true){
+        turn = false;
+    }else{
+        turn = true;
+    }  displayTurn()
+}
 function disableCards(){
             firstCard.removeEventListener('click', flipCard);
             secondCard.removeEventListener('click', flipCard);
+            
+           if(turn == true){
             x++
-            player.innerHTML = "you:" + x;
+            player.innerHTML = "You:" + " " + x;
+            
+           
+
+           } else if(turn == false){
+            y++
+            CPU.innerHTML = "CPU:" + "" + y;
+           }
 
             resetBoard();
 }
@@ -46,7 +63,7 @@ function unflipCards(){
             firstCard.classList.remove('flip');
             secondCard.classList.remove('flip');
            resetBoard();
-            }, 150);
+            }, 1000);
     
 }
 
@@ -57,8 +74,31 @@ function resetBoard() {
 
 (function shuffle() {
     cards.forEach(card => {
-        let randomPos =  Math.floor(Math.random()*12);
+        let randomPos =  Math.floor(Math.random()*16);
         card.style.order = randomPos;
     });
 })();
+
+function NPC(){
+    i = Math.floor(Math.random()*16);
+    document.getElementById(i).click();
+    setTimeout(function() {
+       i = Math.floor(Math.random()*16);
+    document.getElementById(i).click();
+       }, 1000);
+    
+
+}
+function displayTurn(){
+        if(turn == true){
+            CPU.classList.remove('turn');
+            player.classList.add('turn');
+        }else if (turn == false){
+            player.classList.remove('turn');
+            CPU.classList.add('turn');  
+        }
+    }
+function reset(){ window.location.reload();}
+resetBtn.forEach(btn => btn.addEventListener('click', reset ))
 cards.forEach(card => card.addEventListener('click', flipCard))
+NPC();
